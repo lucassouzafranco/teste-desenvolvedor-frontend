e10868ffe5342369babf19bec2ef56e80166dbb4
Lucas Souza <lucassouzafranco1@gmail.com>
2024-04-16 20:19:08 -0300

    ✨ feat(menu icons): button to collapse side bar, logo and profile icon

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,9 +1,25 @@
 import './Menu.css';
+import { HiMenuAlt2 } from "react-icons/hi";
+import { IoPerson } from "react-icons/io5";
+import medical_cross_img from '../../assets/medical_cross_img.png';
 
 function Menu() {
   return (
     <div className='MenuContainer'>
-      <div className='MenuContent'>PHARMA.LIB</div>
+      <div className='MenuContent'>
+        <button className='SideBarButton'>
+          <HiMenuAlt2 className='MenuIcon'/>
+        </button>
+        <div className='TitleAndLogo'>
+          PHARMA.LIB 
+          <img 
+          src={medical_cross_img} 
+          alt='medical cross' 
+          className='Logo'
+          />
+        </div>
+        <IoPerson className='MenuIcon'/>
+      </div>
     </div>
   );
 }
